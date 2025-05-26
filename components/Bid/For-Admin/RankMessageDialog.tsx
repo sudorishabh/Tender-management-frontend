69dd273b20267c41e554ec3fdcc08d108a276cbd
Rishabh Negi <Rishabh.Negi_i@TERI.RES.IN>
2025-05-26 13:47:44 +0530

    rankMessageDialog un-used import removed

--- components/Bid/For-Admin/RankMessageDialog.tsx
+++ components/Bid/For-Admin/RankMessageDialog.tsx
@@ -15,10 +15,7 @@ import { Textarea } from "@/components/ui/textarea";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 // import { useSendMailMutation } from "@/Redux/inform/informApi";
-import {
-  useSendCustomMailMutation,
-  useSendMailMutation,
-} from "@/Redux/inform/informApi";
+import { useSendMailMutation } from "@/Redux/inform/informApi";
 import { toast } from "sonner";
 
 const RankMessageDialog = ({
